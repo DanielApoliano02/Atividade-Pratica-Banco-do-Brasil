40d78e52cd4e5547e8a4d3e04c4d4db75283d245
DanielApoliano02 <134645514+DanielApoliano02@users.noreply.github.com>
2023-05-26 07:28:58 -0300

    Create Endereco.java

---
+++ Programa Java/Endereco.java
@@ -0,0 +1,62 @@
+enum TipoEndereco {
+    COMERCIAL, RESIDENCIAL
+}
+
+// Classe base para endereço
+class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private TipoEndereco tipoEndereco;
+
+    public Endereco(String logradouro, String bairro, String cep, String cidade, TipoEndereco tipoEndereco) {
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+}
+
+
